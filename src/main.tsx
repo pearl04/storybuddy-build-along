6a2dfad483dd45534f3474ce6257622bed8c6424
pearl04 <pearlprakash04@gmail.com>
2025-04-29 09:31:07 +0100

    posthog setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,19 +1,20 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.tsx';
+import './index.css';
 import posthog from 'posthog-js';
-import React from 'react'
-import ReactDOM from 'react-dom/client'
 
-// Initialize PostHog
+// Initialize PostHog (EU instance)
 posthog.init('phc_wQOC4GGisd0h37WXZ4WAIvRyRH2aUfpC5OgbSRdGeBh', {
   api_host: 'https://eu.i.posthog.com',
 });
+
+// Track pageview
 posthog.capture('$pageview');
 
+// Only render once
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
-createRoot(document.getElementById("root")!).render(<App />);
+  </React.StrictMode>
+);
